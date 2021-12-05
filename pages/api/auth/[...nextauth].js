a916203e6e97b01d57cf2b4d844e48cd985fb372
Eren Akel <eren.akel.46@gmail.com>
2021-12-05 10:47:07 +0300

    login url with search parameters added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { LOGIN_URL } from "../../../lib/spotify"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,7 +8,7 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      authorization:
+      authorization: LOGIN_URL
     }),
     // ...add more providers here
   ],
