9938329c855f2b2689d03b54c1abcc986065f12a
Eren Akel <eren.akel.46@gmail.com>
2021-12-05 16:27:53 +0300

    spotifyApi used in sidebar component and playlist data of user got

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,10 +8,23 @@ import {
 } from "@heroicons/react/outline";
 
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import useSpotify from '../hooks/useSpotify';
 
 function Sidebar() {
+    const spotifyApi = useSpotify();
 
     const { data: session, status } = useSession();
+
+    const [playlists, setPLaylists] = useState([]);
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPLaylists(data.body.items);
+            });
+        }
+    }, [session, spotifyApi]);
 
     return (
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
@@ -44,10 +57,13 @@ function Sidebar() {
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900" />
 
-
-                <p className="cursor-pointer hover:text-white">
-                    Playlist name...
-                </p>
+                {
+                    playlists.map((playlist) => (
+                        <p key={playlist.id} className="cursor-pointer hover:text-white">
+                            {playlist.name}
+                        </p>
+                    ))
+                }
             </div>
         </div>
     )
