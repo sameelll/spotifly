7dd266320fc442b544ccf5672abe237676396e9b
Eren Akel <eren.akel.46@gmail.com>
2021-12-06 16:40:02 +0300

    Finished with active device problem

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,9 +4,12 @@ import {
     SearchIcon,
     LibraryIcon,
     PlusCircleIcon,
-    HeartIcon,
     RssIcon
 } from "@heroicons/react/outline";
+
+import {
+    HeartIcon
+} from "@heroicons/react/solid";
 // Session provider
 import { signOut, useSession } from "next-auth/react";
 // Hooks
@@ -61,12 +64,12 @@ function Sidebar() {
                     <PlusCircleIcon className="h-5 w-5" />
                     <p>Your Episodes</p>
                 </button>
-                <button className="flex items-center space-x-2 hover:text-white">
-                    <HeartIcon className="h-5 w-5" />
+                <button className="flex items-center space-x-2  hover:text-white">
+                    <HeartIcon className="h-5 w-5 text-red-600" />
                     <p>Liked Songs</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <RssIcon className="h-5 w-5" />
+                    <RssIcon className="h-5 w-5 text-green-500" />
                     <p>Search</p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900" />
